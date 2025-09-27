6258e0ecb11aca894b8338b3dacc1dabfdcae7e3
mouli2005768 <moulibrahma2005@gmail.com>
2025-09-27 10:58:49 +0530

    changed the rootcontroller

--- src/main/java/klu/config/CorsConfig.java
+++ src/main/java/klu/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:5173/frontapp2")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
